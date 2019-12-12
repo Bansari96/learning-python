64332be212ec0b297369684411a2863cc1730d27
Bansari Patel <Bansari.Patel@cantire.com>
2019-12-12 16:35:30 -0500

    parsing xml modified by adding ElementTree library

--- xmlparsing.py
+++ xmlparsing.py
@@ -1,15 +1,19 @@
 # Example for parsing and processing XML 
 
 import xml.dom.minidom
+from xml.etree import ElementTree
 
 def main():
+
+    tree=ElementTree.parse("samplexml.xml")
     doc=xml.dom.minidom.parse("samplexml.xml")
 
     print(doc.nodeName)
-    print(doc.firstChild.tagName)
+    root=tree.getroot()
+    print("Name: " + root.find("firstname").text)
 
     skills=doc.getElementsByTagName("skill")
-    print("%d skills: " %skills.length)
+    print("Total skills: %d" %skills.length)
     for skill in skills:
         print(skill.getAttribute("name"))
     print("------------\n")
@@ -18,10 +22,10 @@ def main():
     newSkill.setAttribute("name","python")
     doc.firstChild.appendChild(newSkill)
     skills=doc.getElementsByTagName("skill")
-    print("%d skills: " %skills.length)
+    print("After new Skill added")
+    print("Total skills: %d" %skills.length)
     for skill in skills:
         print(skill.getAttribute("name"))
-
 
 
 if __name__ == "__main__":
